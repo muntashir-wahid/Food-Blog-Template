d9c72ff35e537bd42487b34e3641ba82a0d87e54
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-03-09 19:13:04 +0600

    added reveal animation

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const [ container1El, 
+        container2El, 
+        container3El,
+        ...restEl
+    ] = [...document.querySelector('main').children];
+
+const foodContainers = [container1El, container2El, container3El];
+
+const revealFood = function(entries, observer) {
+    const [entry] = entries;
+    console.log(entry);
+    if(entry.isIntersecting) {
+        const [ ,classList] = entry.target.classList;
+        if (!classList) return;
+        entry.target.classList.remove(classList);
+        observer.unobserve(entry.target);
+    }
+}
+
+const containerObserver = new IntersectionObserver(revealFood, {
+    root: null,
+    threshold: 0,
+});
+
+foodContainers.forEach((container, index)=> {
+    containerObserver.observe(container);
+    if (index % 2 === 0) {
+        container.classList.add('move-right');
+    } else {
+        container.classList.add('move-left');
+    }
+})
